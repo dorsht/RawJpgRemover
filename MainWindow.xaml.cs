ab03936fdff6348f8e6ff2f2cd983cf16786eff2
Dor Shtarker <34947168+dorsht@users.noreply.github.com>
2021-04-14 12:24:15 +0300

    Adding file extensions scarpping from web.
    Instead of using only hard coded RAW file extensions, now I'll scrap them on runtime.
    If internet connection isn't available for some reason, I'll use the hard coded RAW file extensions list which already defined in the code.
    
    Also, fixed some issue with compiling, since it needed StampTool.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,47 +1,82 @@
 ﻿using System.Windows;
 using System.IO;
 using System.Windows.Forms;
-using MessageBox = System.Windows.Forms.MessageBox;
 using System.Windows.Controls;
 using System.Linq;
+using System.Net;
+using MessageBox = System.Windows.Forms.MessageBox;
+using System.Collections.Generic;
 
 namespace RawJpegRemover
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
-        private static DirectoryInfo jpgDir, rawDir;
-        private static int totalDeleted;
+        private DirectoryInfo jpgDir, rawDir;
+        private int totalDeleted;
+        private const int CharsToJumpOver = 2;
+        private const int ExtensionLength = 4; // With dot
+        private const int MinimalNeededLength = 6;
         private enum TypeSelect { JPG, RAW };
 
         private void InitializeRawSelector()
         {
-            // List taken from: https://fileinfo.com/filetypes/camera_raw 
-            string[] rawOptions = {".3FR", ".ARI", ".ARW", ".BAY", ".CR2", ".CR3", ".CRW", ".CS1",
+            string[] rawFileExtensions;
+            try
+            {
+                using (WebClient client = new WebClient())
+                {
+                    int index;
+                    List<string> fileExtensions = new List<string>();
+                    string [] delimiters = { "<tbody>" };
+                    string htmlCode = client.DownloadString("https://fileinfo.com/filetypes/camera_raw");
+                    string [] htmlCodeSplitted = htmlCode.Split(delimiters, System.StringSplitOptions.None);
+                    string [] splittedLines = htmlCodeSplitted[1].Split('\n');
+                    foreach (string splittedLine in splittedLines)
+                    {
+                        index = splittedLine.IndexOf("\">.");
+                        if (index > 0 && splittedLine.Length > index + MinimalNeededLength)
+                        {
+                            // Adding charsToJumpOver to index since I got the index of the " and I want to jump over ">.
+                            fileExtensions.Add(splittedLine.Substring(index + CharsToJumpOver, ExtensionLength));
+                        }
+                    }
+                    fileExtensions.Sort();
+                    rawFileExtensions = fileExtensions.ToArray();
+                }
+            }
+            catch (WebException e)
+            {
+                // List taken from: https://fileinfo.com/filetypes/camera_raw 
+                rawFileExtensions = new string[]{".3FR", ".ARI", ".ARW", ".BAY", ".CR2", ".CR3", ".CRW", ".CS1",
                                     ".CXI", ".DCR", ".DNG", ".EIP", ".ERF", ".FFF", ".IIQ", ".J6I",
                                     ".K25", ".KDC", ".MEF", ".MFW", ".MOS", ".MRW", ".NEF", ".NRW",
                                     ".ORF", ".PEF", ".RAF", ".RAW", ".RW2", ".RWL", ".RWZ", ".SR2",
                                     ".SRF", ".SRW", ".X3F"};
-            foreach(string rawOption in rawOptions)
+                System.Console.WriteLine(e.ToString()); // For debugging only.
+            }
+            foreach (string rawOption in rawFileExtensions)
+            {
                 rawTypeSelector.Items.Add(rawOption);
+            }
         }
         public MainWindow()
         { 
             InitializeComponent();
             InitializeRawSelector();
-            totalDeleted = 0;
         }
                 
         private void GetFolderSelect(TextBlock textBlock, TypeSelect select)
         {
             FolderBrowserDialog folderBrowser = new FolderBrowserDialog();
             if (select == TypeSelect.JPG)
+            {
                 folderBrowser.RootFolder = System.Environment.SpecialFolder.MyComputer;
+            }
             else if (jpgDir != null && select == TypeSelect.RAW)
+            {
                 // Folder browser will start from the same directory that the user set for Jpg files
                 folderBrowser.SelectedPath = jpgDir.FullName;
+            }
             if (folderBrowser.ShowDialog() == System.Windows.Forms.DialogResult.OK)
             {
                 if (select == TypeSelect.JPG)
@@ -50,13 +85,16 @@ namespace RawJpegRemover
                     textBlock.Text = "The selected folder is: " + jpgDir.FullName;
                 }
                 // Using else if instead if, if I'll need to add another file type
-                else if (select == TypeSelect.RAW) 
+                else if (select == TypeSelect.RAW)
                 {
                     rawDir = new DirectoryInfo(folderBrowser.SelectedPath);
                     textBlock.Text = "The selected folder is: " + rawDir.FullName;
                 }
             }
-            else MessageBox.Show("Problem occured during folder selection, please try again.");
+            else
+            {
+                MessageBox.Show("Problem occured during folder selection, please try again.");
+            }
         }
 
         private void JpgFolderSelectButton_Click(object sender, RoutedEventArgs e)
@@ -91,23 +129,31 @@ namespace RawJpegRemover
             return retVal;
         }
 
-        private static bool JpgFileExists(FileInfo[] jpgFiles, string jpgFileNameVar1, string jpgFileNameVar2)
+        private bool JpgFileExists(FileInfo[] jpgFiles, string jpgFileNameVar1, string jpgFileNameVar2)
         {
             foreach (FileInfo jpgFile in jpgFiles)
+            {
                 if (jpgFile.Name.Equals(jpgFileNameVar1) || jpgFile.Name.Equals(jpgFileNameVar2))
+                {
                     return true;
+                }
+            }
             return false;
         }
 
-        private static bool RawFileExists(FileInfo[] rawFiles, string rawFileNameVar)
+        private bool RawFileExists(FileInfo[] rawFiles, string rawFileNameVar)
         {
             foreach (FileInfo rawFile in rawFiles)
+            {
                 if (rawFile.Name.Equals(rawFileNameVar))
+                {
                     return true;
+                }
+            }
             return false;
         }
 
-        private static void DeleteRawFilesByJpgFiles (string rawFileType)
+        private void DeleteRawFilesByJpgFiles (string rawFileType)
         {
             FileInfo[] jpgFiles = jpgDir.EnumerateFiles()
                 .Where(x => ".jpg".Contains(x.Extension.ToLower()))
@@ -116,7 +162,8 @@ namespace RawJpegRemover
                 .Where(x => rawFileType.Contains(x.Extension))
                 .ToArray();
             if (jpgFiles.Length > 0)
-            { // I'll not iterate over Raw files array if there're no Jpg files.
+            { 
+                // I won't iterate over Raw files array if there are no Jpg files.
                 foreach (FileInfo rawFile in rawFiles)
                 {
                     string rawFileName = rawFile.Name;
@@ -124,7 +171,10 @@ namespace RawJpegRemover
                     string noExtensionFileName = rawFileName.Substring(0, dotPos);
                     string jpgFileNameVar1 = noExtensionFileName + ".jpg",
                         jpgFileNameVar2 = noExtensionFileName + ".JPG";
-                    if (JpgFileExists(jpgFiles, jpgFileNameVar1, jpgFileNameVar2)) continue;
+                    if (JpgFileExists(jpgFiles, jpgFileNameVar1, jpgFileNameVar2))
+                    {
+                        continue;
+                    }
                     else
                     {
                         rawFile.Delete();
@@ -134,7 +184,7 @@ namespace RawJpegRemover
             }
         }
 
-        private static void DeleteJpgFilesByRawFiles(string rawFileType)
+        private void DeleteJpgFilesByRawFiles(string rawFileType)
         {
             FileInfo[] jpgFiles = jpgDir.EnumerateFiles()
                 .Where(x => ".jpg".Contains(x.Extension.ToLower()))
@@ -143,14 +193,18 @@ namespace RawJpegRemover
                 .Where(x => rawFileType.Contains(x.Extension))
                 .ToArray();
             if (rawFiles.Length > 0)
-            { // I'll not iterate over Jpg files array if there're no Raw files.
+            { 
+                // I won't iterate over Jpg files array if there are no Raw files.
                 foreach (FileInfo jpgFile in jpgFiles)
                 {
                     string JPGFileName = jpgFile.Name;
                     int dotPos = JPGFileName.LastIndexOf(".");
                     string noExtensionFileName = JPGFileName.Substring(0, dotPos);
                     string rawFileNameVar = noExtensionFileName + rawFileType;
-                    if (RawFileExists(rawFiles, rawFileNameVar)) continue;
+                    if (RawFileExists(rawFiles, rawFileNameVar))
+                    {
+                        continue;
+                    }
                     else
                     {
                         jpgFile.Delete();
@@ -170,15 +224,19 @@ namespace RawJpegRemover
                 string rawFileType = rawTypeSelector.Text;
                 totalDeleted = 0;
                 if (rawByJpg.IsChecked.Value)
+                {
                     DeleteRawFilesByJpgFiles(rawFileType);
+                }
                 else if (jpgByRaw.IsChecked.Value)
+                {
                     DeleteJpgFilesByRawFiles(rawFileType);
+                }
                 else if (bothDelete.IsChecked.Value) // Both of them checked
                 {
                     DeleteRawFilesByJpgFiles(rawFileType);
                     DeleteJpgFilesByRawFiles(rawFileType);
                 }
-                MessageBox.Show("Operation completed, totally " + totalDeleted + " files deleted.");
+                MessageBox.Show(string.Format("Operation completed, totally {0} files deleted.", totalDeleted));
                 startOperation.Content = originalText;
                 startOperation.IsEnabled = true;
             }
